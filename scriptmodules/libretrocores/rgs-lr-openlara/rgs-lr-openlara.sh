232c157ce8dad8f15ddbcbafd44b57ae8e95ad75
V0rt3x667 <al-g0l@outlook.com>
2020-11-30 23:59:24 +0000

    Update rgs-lr-openlara.sh - Fixed issue with port not being added during configuration run.

--- scriptmodules/libretrocores/rgs-lr-openlara/rgs-lr-openlara.sh
+++ scriptmodules/libretrocores/rgs-lr-openlara/rgs-lr-openlara.sh
@@ -6,7 +6,7 @@
 
 archrgs_module_id="rgs-lr-openlara"
 archrgs_module_desc="Tomb Raider Libretro Core"
-archrgs_module_help="ROM Extensions: .phd .psx .tr2\n\nPlease copy your Tomb Raider data files to $romdir/ports/tombraider before running the game."
+archrgs_module_help="ROM Extensions: .phd .psx .tr2 .tr4\n\nPlease copy your Tomb Raider data files to $romdir/ports/tombraider before running the game."
 archrgs_module_licence="BSD https://raw.githubusercontent.com/libretro/OpenLara/master/LICENSE"
 archrgs_module_section="libretrocores"
 
@@ -21,12 +21,12 @@ function remove_rgs-lr-openlara() {
 function configure_rgs-lr-openlara() {
   setConfigRoot "ports"
 
-  mkRomDir "tomb_raider"
+  mkRomDir "ports/tomb_raider"
 
-  addPort "$md_id" "tomb_raider" "$md_inst/openlara_libretro.so"
+  addPort "$md_id" "tomb_raider" "Tomb Raider" "$md_inst/openlara_libretro.so"
 
   [[ "$md_mode" == "remove" ]] && return
 
-  ensureSystemretroconfig "tomb_raider"
+  ensureSystemretroconfig "ports/tomb_raider"
 }
 
